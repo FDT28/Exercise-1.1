d27177a5d297293bd960c902bdb98581ab0e9eef
Fabio D. Toguchi <144295657+FDT28@users.noreply.github.com>
2023-10-26 03:09:57 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,8 @@
+//Introduction Excercise
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
